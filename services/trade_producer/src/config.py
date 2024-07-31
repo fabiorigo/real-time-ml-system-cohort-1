3bedfea11fab79c58d22955cbcdefa1cbcf62633
Fabio Scramim Rigo <fabio.s.rigo@protonmail.com>
2024-07-31 09:48:54 +0200

    Added pydantic-settings to manage the configuration and using envvars for differentiating dev and docker

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,15 +1,24 @@
-# kafka_broker_address = 'localhost:19092'
-kafka_broker_address = 'redpanda-0:9092'
-kafka_topic_name = 'trades'
-product_ids = [
-    'BTC/USD',
-    'ETH/USD',
-    'USDT/USD',
-    'SOL/USD',
-    'USDC/USD',
-    'XRP/USD',
-    'DOGE/USD',
-    'ADA/USD',
-    'TRX/USD',
-    'AVAX/USD',
-]
+import os
+from typing import List
+from dotenv import load_dotenv, find_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv(find_dotenv())
+
+class Config(BaseSettings):
+    kafka_broker_address: str = os.environ['KAFKA_BROKER_ADDRESS']
+    kafka_topic_name: str = 'trades'
+    product_ids: List[str] = [
+        'BTC/USD',
+        'ETH/USD',
+        'USDT/USD',
+        'SOL/USD',
+        'USDC/USD',
+        'XRP/USD',
+        'DOGE/USD',
+        'ADA/USD',
+        'TRX/USD',
+        'AVAX/USD',
+    ]
+
+config = Config()
